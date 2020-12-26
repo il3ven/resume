ddd5a349888bfe91f3038d41e36501a5e692c732
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-12-27 02:02:32 +0530

    Added script + Restructuring

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 /* A script to update the "last updated tag" in index.html */
 
-var fs = require("fs"),
-  path = require("path"),
-  filePath = path.join(__dirname, "index.html");
+var fs = require("fs");
+var path = require("path");
+var filePath = path.join(__dirname, "index.html");
+var parse = require("node-html-parser").parse;
 
 fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
   if (!err) {
-    htmlDoc = document.createElement("html");
-    htmlDoc.innerText = data;
+    const html = parse(data);
 
-    htmlDoc.getElementsByClassName("last-update");
-    console.log(htmlDoc.textContent);
+    const date = new Date().toLocaleString("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+
+    html
+      .querySelector(".last-update")
+      .setAttribute("#text", `Last Updated on ${date}`);
+
+    fs.writeFile("build/index.html", html.innerHTML, function (err) {
+      if (err) {
+        console.error("Error Occured", err);
+      }
+
+      console.log("File written");
+    });
   } else {
     console.log(err);
   }
